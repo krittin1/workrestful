405c3b15fd205630e83cc77849c4452a39bf2610
krittin1 <krittin.kan1@ku.th>
2020-08-05 13:31:09 +0700

    :package: [NEW]: edit UserController 3

--- demo/demo/src/main/java/com/example/demo/UserController.java
+++ demo/demo/src/main/java/com/example/demo/UserController.java
@@ -11,7 +11,10 @@ public class UserController {
 
     @GetMapping("/users")
     // TODO: step_02 is done
-    public  @ResponseBody List<UsersResponse> getAllUser(@RequestParam(value="page", required = false) int page, @RequestParam(value="item_per_page", defaultValue = "10", required = false) int item_per_page) {
+    //       there have some bug to fix.
+    public  List<UsersResponse> getAllUser(
+             @RequestParam int page
+            ,int item_per_page) {
         List<UsersResponse> usersResponseList = new ArrayList<>();
         usersResponseList.add(new UsersResponse(1, "User 1"));
         usersResponseList.add(new UsersResponse(2, "User 2"));

107bd0d98306176c97b9790fe59deeb6c7b08537
Arty                Kanok <krittin.kan1@ku.th>
2020-08-05 13:15:19 +0700

    :package: [NEW]: add new UserController endpoint

--- demo/demo/src/main/java/com/example/demo/UserController.java
+++ demo/demo/src/main/java/com/example/demo/UserController.java
@@ -1,16 +1,26 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+
 public class UserController {
 
     @GetMapping("/users")
-    public UsersResponse[] getAllUser() {
-        UsersResponse[] usersResponses = new UsersResponse[2];
-        usersResponses[0] = new UsersResponse(1, "User 1");
-        usersResponses[1] = new UsersResponse(2, "User 2");
-        return usersResponses;
+    // TODO: step_02 is done
+    public  @ResponseBody List<UsersResponse> getAllUser(@RequestParam(value="page", required = false) int page, @RequestParam(value="item_per_page", defaultValue = "10", required = false) int item_per_page) {
+        List<UsersResponse> usersResponseList = new ArrayList<>();
+        usersResponseList.add(new UsersResponse(1, "User 1"));
+        usersResponseList.add(new UsersResponse(2, "User 2"));
+        usersResponseList.add(new UsersResponse(3, "User 3"));
+        return usersResponseList;
+    }
+
+    @GetMapping("/users/{id}")
+    public UsersResponse getUserById(@PathVariable int id) {
+        return new UsersResponse(id, "User " + id);
     }
 }
